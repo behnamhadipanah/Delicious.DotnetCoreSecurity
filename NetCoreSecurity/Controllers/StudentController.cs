b3d08968093e2fae0e00f076c0357fe263f0799b
b.hadipanah <b.hadipanah@adonistech.ir>
2025-01-28 15:01:22 +0330

    added sln project

---
+++ NetCoreSecurity/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreSecurity.Models.DataServices;
+using NetCoreSecurity.Models.Student;
+
+namespace NetCoreSecurity.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentDataContext _context;
+
+        public StudentController(StudentDataContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(new List<CourseGrade>());
+        }
+        [HttpGet]
+        public IActionResult AddGrade()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGrade(CourseGrade model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            model.CreatedDate = DateTime.Now;
+
+            _context.Grades.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(StudentController.Index), "Student");
+        }
+        [HttpGet]
+        public IActionResult Classifications()
+        {
+            var classifications = new List<string>()
+            {
+                "Freshman",
+                "Sophomore",
+                "Junior",
+                "Senior"
+            };
+
+            return View(classifications);
+        }
+    }
+}
---
+++ NetCoreSecurity/Models/DataServices/StudentDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreSecurity.Models.Student;
+using System.Collections.Generic;
+
+namespace NetCoreSecurity.Models.DataServices
+{
+    public class StudentDataContext : DbContext
+    {
+        public DbSet<CourseGrade> Grades { get; set; }
+
+        public StudentDataContext(DbContextOptions<StudentDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
