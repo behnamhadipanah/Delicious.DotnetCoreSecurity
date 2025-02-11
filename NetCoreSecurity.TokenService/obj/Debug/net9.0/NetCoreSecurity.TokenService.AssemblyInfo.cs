3d6c5129a1201a716fa6a2ade94f2e50f463b05e
b.hadipanah <b.hadipanah@adonistech.ir>
2025-02-11 12:59:43 +0330

    update staged

--- NetCoreSecurity.TokenService/obj/Debug/net9.0/NetCoreSecurity.TokenService.AssemblyInfo.cs
+++ NetCoreSecurity.TokenService/obj/Debug/net9.0/NetCoreSecurity.TokenService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCoreSecurity.TokenService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac6a64d0fe86a3ec7b011904ce375c63865638c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3de668a7c9f1726cbc4923295a4794b932bf6cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCoreSecurity.TokenService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCoreSecurity.TokenService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
